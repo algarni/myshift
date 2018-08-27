e7c689d118be1ec3c9eb6e7ffef62016744c584e
Mohammad Algarni <m.t.algarni@gmail.com>
2018-08-27 07:58:09 +0300

    Add Group & Site models

--- app/models.py
+++ app/models.py
@@ -9,3 +9,20 @@ class Employee(db.Model):
 
     def __repr__(self):
         return '<Employee {}>'.format(self.name)
+
+
+class Group(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+
+
+class Site(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+
+
+class ShiftPlan(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Colunm(db.String(50))
+
+
